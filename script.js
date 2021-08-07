821a1bd8a1ce80cb88f4cd8a2eb54cc4b5ecb760
Ven Gallardo <letskeddotcom@gmail.com>
2021-08-08 03:55:42 +0800

    added some states and sample details

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// landing page
+
 const toggle = document.getElementById('nav-btn');
 const page = document.getElementById('page');
 
@@ -5,4 +7,33 @@ toggle.onclick = function(){
     // console.log("Active");
      toggle.classList.toggle('active');
      page.classList.toggle('active');
+}
+
+
+// navigation 
+
+const about = document.getElementById('about');
+const project = document.getElementById('project');
+const skills = document.getElementById('skills');
+
+const aboutC = document.getElementById('about-c');
+
+about.onclick = function(){
+    aboutC.classList.add('show');
+    project.classList.remove('show');
+    skills.classList.remove('show');
+    about.classList.add('toggle');
+
+}
+project.onclick = function(){
+    aboutC.classList.remove('show');
+    project.classList.add('show');
+    skills.classList.remove('show');
+
+}
+skills.onclick = function(){
+    aboutC.classList.remove('show');
+    project.classList.remove('show');
+    skills.classList.add('show');
+
 }
